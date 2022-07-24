5c51c6aba51fd3ab9d3a677aafda99268fcabe40
Robert Schwindler <robertmichaelschwindler@gmail.com>
2022-07-24 06:37:14 -0400

    WeekTwo Finish

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+<<<<<<< Updated upstream
 import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
+=======
+import { Routes, Route } from 'react-router-dom';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+import CampsiteDetailPage from './pages/CampsiteDetailPage';
+>>>>>>> Stashed changes
 import './App.css';
+import AboutPage from './pages/AboutPage';
 
 function App() {
+<<<<<<< Updated upstream
   return (
     <div className="App">
       <header className="App-header">
@@ -53,6 +65,21 @@ function App() {
       </header>
     </div>
   );
+=======
+    return (
+        <div className='App'>
+            <Header />
+            <Routes>
+                <Route path='/' element={<HomePage />} />
+                <Route path='contact' element={<ContactPage />} />
+                <Route path='directory' element={<CampsitesDirectoryPage />} />
+                <Route path='directory/:campsiteId' element={<CampsiteDetailPage />} />
+                <Route path='about' element={<AboutPage />} />
+            </Routes>
+            <Footer />
+        </div>
+    );
+>>>>>>> Stashed changes
 }
 
 export default App;
---
+++ src/features/campsites/CampsiteCard.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Card, CardImg, CardTitle, CardImgOverlay } from 'reactstrap';
+
+const CampsiteCard = ({campsite}) => {
+
+    const { id, image, name } = campsite;
+
+    return (
+            <Link to={`${id}`}>
+                <Card>
+                    <CardImg width='100%' src = {image} alt = {name} />
+                    <CardImgOverlay>
+                        <CardTitle>{name}</CardTitle>
+                    </CardImgOverlay>
+                </Card>
+            </Link>
+    );
+}
+
+export default CampsiteCard;
