58b2dfb82b699fe1f1401e6cd44161338b35fdec
Robert Schwindler <robertmichaelschwindler@gmail.com>
2022-07-25 06:53:34 -0400

    AnimationBrokenLogo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
-<<<<<<< Updated upstream
 import React from 'react';
-import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
-=======
 import { Routes, Route } from 'react-router-dom';
 import ContactPage from './pages/ContactPage';
 import HomePage from './pages/HomePage';
@@ -10,62 +7,11 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
->>>>>>> Stashed changes
 import './App.css';
 import AboutPage from './pages/AboutPage';
 
 function App() {
-<<<<<<< Updated upstream
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
-=======
+
     return (
         <div className='App'>
             <Header />
@@ -79,7 +25,6 @@ function App() {
             <Footer />
         </div>
     );
->>>>>>> Stashed changes
 }
 
 export default App;
