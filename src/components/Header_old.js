01ff7e80e384f2c392f0c90eb14e8ba7af03eca3
Robert Schwindler <robertmichaelschwindler@gmail.com>
2022-07-27 06:42:31 -0400

    WeekThreeExercises

--- src/components/Header_old.js
+++ src/components/Header_old.js
@@ -8,7 +8,7 @@ import {
     NavItem,
 } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
-import NucampLogo from '../app/assets/img/logo.png';
+//import NucampLogo from '../app/assets/img/logo.png';
 
 const Header = () => {
     const [menuOpen, setMenuOpen] = useState(false);
@@ -16,7 +16,7 @@ const Header = () => {
     return (
         <Navbar dark color='primary' sticky='top' expand='md'>
             <NavbarBrand className='ms-5' href='/'>
-            <img src={NucampLogo} alt='nucamp logo' className='float-start' />
+                <img src={NucampLogo} alt='nucamp logo' className='float-start' />
                 <h1 className='mt-1'>NuCamp</h1>
             </NavbarBrand>
             <NavbarToggler onClick={() => setMenuOpen(!menuOpen)}/>
@@ -49,4 +49,3 @@ const Header = () => {
 };
 
 export default Header;
-//<img src={NucampLogo} alt='nucamp logo' className='float-start' />
---
+++ src/utils/validateContactForm.js
@@ -0,0 +1,41 @@
+
+
+export const validateContactForm = (values) => {
+    const errors = {};
+
+        if (!values.firstName) {
+            errors.firstName = 'Required';
+        }
+        else if (values.firstName < 2){
+            errors.firstName = 'Must be at least 2 characters.';
+        }
+        else if (values.firstName > 15){
+            errors.firstName = 'Must be 15 characters or less';
+        }
+
+        if (!values.lastName) {
+            errors.lastName = 'Required';
+        } 
+        else if (values.lastName < 2){
+            errors.lastName = 'Must be at least 2 characters.';
+        } 
+        else if (values.lastName > 20){
+            errors.lastName = 'Must be 20 characters or less';
+        }
+
+    
+
+    const reg = /^\d+$/;
+
+        if (!reg.test(values.phoneNum)) {
+            errors.phoneNum = 'The phone number should contain only numbers';
+        } else if (values.phoneNum < 9){
+            errors.phoneNum = 'Must be at least 9 numbers.';
+        }
+
+        if (!values.email.includes('@')) {
+            errors.email = 'Email must include a @';
+        }
+        
+    return errors;    
+};
